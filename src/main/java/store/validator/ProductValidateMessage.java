fe96ea82c967bed46c3401acb0eb46f8485bdb56
lvalentine6 <ysr1227@naver.com>
2024-11-10 01:37:10 +0900

    feat: add domain validate message

---
+++ src/main/java/store/validator/ProductValidateMessage.java
@@ -0,0 +1,18 @@
+package store.validator;
+
+public enum ProductValidateMessage {
+	INVALID_NAME("[ERROR] 상품 이름은 비어있을수 없습니다."),
+	INVALID_MIN_PRICE("[ERROR] 상품 가격은 0보다 커야 합니다."),
+	INVALID_MIN_QUANTITY("[ERROR] 상품 수량은 0과 같거나 커야 합니다."),
+	;
+
+	private final String message;
+
+	ProductValidateMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ src/main/java/store/validator/StockValidateMessage.java
@@ -0,0 +1,16 @@
+package store.validator;
+
+public enum StockValidateMessage {
+	INVALID_DUPLICATE_PROMOTIONS("[ERROR] 하나의 상품에는 하나의 프로모션만 가능합니다."),
+	;
+
+	private final String message;
+
+	StockValidateMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
