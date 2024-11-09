5cbddeef51fcd29bbf9bcbd475bedf25354165a7
lvalentine6 <ysr1227@naver.com>
2024-11-10 05:53:58 +0900

    feat: add out of stock and duplication check

--- src/main/java/store/domain/product/Stock.java
+++ src/main/java/store/domain/product/Stock.java
@@ -1,7 +1,9 @@
 package store.domain.product;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import store.validator.stock.StockValidateMessage;
@@ -10,20 +12,66 @@ public class Stock {
 	private final List<Product> products;
 
 	private Stock(List<Product> products) {
+		validateDuplicateProduct(products);
 		validateDuplicatePromotion(products);
-		this.products = products;
+		this.products = addOutOfStockVersions(products);
 	}
 
 	public static Stock from(List<Product> products) {
 		return new Stock(products);
 	}
 
+	private void validateDuplicateProduct(List<Product> products) {
+		Map<String, Long> productCount = products.stream()
+			.filter(product -> !product.hasPromotion())
+			.collect(Collectors.groupingBy(Product::getName, Collectors.counting()));
+
+		if (productCount.values().stream().anyMatch(count -> count > 1)) {
+			throw new IllegalArgumentException(StockValidateMessage.INVALID_DUPLICATE_PRODUCT.getMessage());
+		}
+	}
+
 	private void validateDuplicatePromotion(List<Product> products) {
 		Map<String, Long> promotionCount = products.stream()
 			.filter(Product::hasPromotion)
 			.collect(Collectors.groupingBy(Product::getName, Collectors.counting()));
+
 		if (promotionCount.values().stream().anyMatch(count -> count > 1)) {
 			throw new IllegalArgumentException(StockValidateMessage.INVALID_DUPLICATE_PROMOTIONS.getMessage());
 		}
+	}
+
+	private List<Product> addOutOfStockVersions(List<Product> products) {
+		Set<String> hasNormalVersions = findProductsWithNormalVersion(products);
+		List<Product> result = new ArrayList<>();
+		products.forEach(p -> {
+			result.add(p);
+			if (p.hasPromotion() && !hasNormalVersions.contains(p.getName())) {
+				result.add(createOutOfStockVersion(p));
+			}
+		});
+		return result;
+	}
+
+	private Set<String> findProductsWithNormalVersion(List<Product> products) {
+		return products.stream()
+			.filter(p -> !p.hasPromotion())
+			.map(Product::getName)
+			.collect(Collectors.toSet());
+	}
+
+	private Product createOutOfStockVersion(Product product) {
+		return Product.of(
+			new PrimitiveProductInfo(
+				product.getName(),
+				product.getPrice(),
+				0,
+				null),
+			null
+		);
+	}
+
+	public List<Product> getProducts() {
+		return products;
 	}
 }
