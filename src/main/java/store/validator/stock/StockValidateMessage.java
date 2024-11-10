6fe1d46095afa220d9058d029f445db8d0f0b1de
lvalentine6 <ysr1227@naver.com>
2024-11-10 22:55:23 +0900

    test: add validate orderItems test

--- src/main/java/store/validator/stock/StockValidateMessage.java
+++ src/main/java/store/validator/stock/StockValidateMessage.java
@@ -3,6 +3,8 @@ package store.validator.stock;
 public enum StockValidateMessage {
 	INVALID_DUPLICATE_PRODUCT("[ERROR] 중복된 상품은 등록할 수 없습니다."),
 	INVALID_DUPLICATE_PROMOTIONS("[ERROR] 하나의 상품에는 하나의 프로모션만 가능합니다."),
+	INVALID_ORDER_PRODUCT("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
+	INVALID_ORDER_OUT_OF_STOCK("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.")
 	;
 
 	private final String message;
--- src/test/java/store/domain/product/StockTest.java
+++ src/test/java/store/domain/product/StockTest.java
@@ -3,10 +3,15 @@ package store.domain.product;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import store.domain.order.OrderItem;
 import store.domain.promotion.PrimitivePromotionInfo;
 import store.domain.promotion.Promotion;
 import store.validator.stock.StockValidateMessage;
@@ -16,14 +21,6 @@ class StockTest {
 	@Test
 	void validateDuplicateProduct() {
 		//given
-		Promotion promotion1 = Promotion.from(
-			new PrimitivePromotionInfo("탄산1+1", 1, 1, "2024-01-01", "2024-12-31")
-		);
-
-		Promotion promotion2 = Promotion.from(
-			new PrimitivePromotionInfo("탄산2+1", 2, 1, "2024-01-01", "2024-12-31")
-		);
-
 		List<Product> products = List.of(
 			Product.of(new PrimitiveProductInfo("콜라", 1000, 10, ""), null),
 			Product.of(new PrimitiveProductInfo("콜라", 1000, 10, ""), null)
@@ -56,5 +53,181 @@ class StockTest {
 		assertThatThrownBy(() -> Stock.from(products))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage(StockValidateMessage.INVALID_DUPLICATE_PROMOTIONS.getMessage());
+	}
+
+	@DisplayName("각 상품의 재고를 고려하여 주문 가능 여부를 확인한다.")
+	@MethodSource("validStockProvider")
+	@ParameterizedTest(name = "{0}")
+	void validateOrderItems(String description, List<Product> inputProducts, List<OrderItem> inputOrderItems) {
+		//given
+		Stock stock = Stock.from(inputProducts);
+
+		//when & then
+		stock.validateOrderItems(inputOrderItems);
+	}
+
+	@DisplayName("주문이 가능하지 않으면 IllegalStateException 예외가 발생한다.")
+	@MethodSource("invalidStockProvider")
+	@ParameterizedTest(name = "{0}")
+	void validateInvalidOrderItems(String description, List<Product> inputProducts, List<OrderItem> inputOrderItems) {
+		//given
+		Stock stock = Stock.from(inputProducts);
+
+		//when & then
+		assertThatThrownBy(() -> stock.validateOrderItems(inputOrderItems))
+			.isInstanceOf(IllegalStateException.class);
+	}
+
+	static Stream<Arguments> validStockProvider() {
+		return Stream.of(
+			Arguments.of(
+				"프로모션이 없고 일반 재고가 있는 주문",
+				createValidNormalOrderProducts(),
+				List.of(new OrderItem("콜라", 10))
+			),
+			Arguments.of(
+				"프로모션이 있고 프로모션이 유효하며 프로모션 재고가 충분한 주문",
+				createValidPromotionProducts(),
+				List.of(new OrderItem("콜라", 10))
+			),
+			Arguments.of(
+				"프로모션이 있고 프로모션이 유효하지만 프로모션 재고가 부족하고 일반 재고가 있는 주문",
+				createValidPromotionOutOfPromotionStockButNormalStockProducts(),
+				List.of(new OrderItem("콜라", 10))
+			),
+			Arguments.of(
+				"프로모션이 있지만 유효하지 않고 일반 재고가 있는 주문",
+				createValidInvalidPromotionButNormalStockProducts(),
+				List.of(new OrderItem("콜라", 5))
+			)
+		);
+	}
+
+	static Stream<Arguments> invalidStockProvider() {
+		return Stream.of(
+			Arguments.of(
+				"없는 상품을 주문",
+				createValidPromotionOutOfPromotionOutOfNormalStockProducts(),
+				List.of(new OrderItem("커피", 3))
+			),
+			Arguments.of(
+				"프로모션이 있고 프로모션이 유효하지만 프로모션 재고가 부족하고 일반 재고도 부족한 주문",
+				createValidPromotionOutOfPromotionOutOfNormalStockProducts(),
+				List.of(new OrderItem("콜라", 3))
+			),
+			Arguments.of(
+				"프로모션이 있지만 유효하지 않고 일반 재고도 부족한 주문",
+				createValidInvalidPromotionOutOfNormalStockProducts(),
+				List.of(new OrderItem("콜라", 6))
+			),
+			Arguments.of(
+				"프로모션이 없고 일반 재고가 부족한 주문",
+				createValidOutOfNormalPromotionStockProducts(),
+				List.of(new OrderItem("콜라", 2))
+			)
+		);
+	}
+
+	// 프로모션이 없고 일반 재고가 있는 주문
+	private static List<Product> createValidNormalOrderProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 10,
+				null
+			),
+			Product.of(
+				"사이다", 1000, 5,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 있고 프로모션이 유효하며 프로모션 재고가 충분한 주문
+	private static List<Product> createValidPromotionProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 10,
+				Promotion.from(
+					new PrimitivePromotionInfo("탄산1+1", 1, 1, "2024-01-01", "2024-12-31"))
+			),
+			Product.of(
+				"콜라", 1000, 5,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 있고 프로모션이 유효하지만 프로모션 재고가 부족하고 일반 재고가 있는 주문
+	private static List<Product> createValidPromotionOutOfPromotionStockButNormalStockProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 1,
+				Promotion.from(
+					new PrimitivePromotionInfo("탄산1+1", 1, 1, "2024-01-01", "2024-12-31"))
+			),
+			Product.of(
+				"콜라", 1000, 9,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 있지만 유효하지 않고 일반 재고가 있는 주문
+	private static List<Product> createValidInvalidPromotionButNormalStockProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 1,
+				Promotion.from(
+					new PrimitivePromotionInfo("탄산1+1", 1, 1, "2023-01-01", "2023-12-31"))
+			),
+			Product.of(
+				"콜라", 1000, 5,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 있고 프로모션이 유효하지만 프로모션 재고가 부족하고 일반 재고도 부족한 주문
+	private static List<Product> createValidPromotionOutOfPromotionOutOfNormalStockProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 1,
+				Promotion.from(
+					new PrimitivePromotionInfo("탄산1+1", 1, 1, "2024-01-01", "2024-12-31"))
+			),
+			Product.of(
+				"콜라", 1000, 1,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 있지만 유효하지 않고 일반 재고도 부족한 주문
+	private static List<Product> createValidInvalidPromotionOutOfNormalStockProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 1,
+				Promotion.from(
+					new PrimitivePromotionInfo("탄산1+1", 1, 1, "2023-01-01", "2023-12-31"))
+			),
+			Product.of(
+				"콜라", 1000, 5,
+				null
+			)
+		);
+	}
+
+	// 프로모션이 없고 일반 재고가 부족한 주문
+	private static List<Product> createValidOutOfNormalPromotionStockProducts() {
+		return List.of(
+			Product.of(
+				"콜라", 1000, 1,
+				null
+			),
+			Product.of(
+				"사이다", 1000, 5,
+				null
+			)
+		);
 	}
 }
