b0ca58eff23080b86f702c1df36e226dddae1625
lvalentine6 <ysr1227@naver.com>
2024-11-10 22:55:40 +0900

    feat: add validate orderItems feature

--- src/main/java/store/domain/product/Stock.java
+++ src/main/java/store/domain/product/Stock.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import store.domain.order.OrderItem;
+import store.domain.promotion.Promotion;
 import store.validator.stock.StockValidateMessage;
 
 public class Stock {
@@ -73,5 +75,84 @@ public class Stock {
 
 	public List<Product> getProducts() {
 		return products;
+	}
+
+	public void validateOrderItems(List<OrderItem> orderItems) {
+		List<String> orderNames = orderItems.stream()
+			.map(OrderItem::name)
+			.toList();
+
+		validateNames(orderNames);
+
+		for (OrderItem orderItem : orderItems) {
+			validateOrderItem(orderItem);
+		}
+	}
+
+	private void validateNames(List<String> orderNames) {
+		Set<String> productsNames = products.stream()
+			.map(Product::getName)
+			.collect(Collectors.toSet());
+
+		if (!productsNames.containsAll(orderNames)) {
+			throw new IllegalStateException(StockValidateMessage.INVALID_ORDER_PRODUCT.getMessage());
+		}
+	}
+
+	private void validateOrderItem(OrderItem orderItem) {
+		Product promotionProduct = findPromotionProductByName(orderItem.name());
+		Product normalProduct = findNormalProductByName(orderItem.name());
+
+		if (promotionProduct != null) {
+			validatePromotionOrder(promotionProduct, normalProduct, orderItem.quantity());
+			return;
+		}
+
+		validateNormalOrderStock(normalProduct, orderItem.quantity());
+	}
+
+	private Product findPromotionProductByName(String name) {
+		return products.stream()
+			.filter(p -> p.getName().equals(name))
+			.filter(Product::hasPromotion)
+			.findFirst()
+			.orElse(null);
+	}
+
+	private Product findNormalProductByName(String name) {
+		return products.stream()
+			.filter(p -> p.getName().equals(name))
+			.filter(p -> !p.hasPromotion())
+			.findFirst()
+			.orElse(null);
+	}
+
+	private void validatePromotionOrder(Product promotionProduct, Product normalProduct, int quantity) {
+		if (!validatePromotionValidity(promotionProduct)) {
+			validateNormalOrderStock(normalProduct, quantity);
+			return;
+		}
+		int remainQuantity = validatePromotionStock(promotionProduct, quantity);
+		if (remainQuantity > 0) {
+			validateNormalOrderStock(normalProduct, remainQuantity);
+		}
+	}
+
+	private boolean validatePromotionValidity(Product promotionProduct) {
+		Promotion promotion = promotionProduct.getPromotion();
+		return promotion.isValidPromotion();
+	}
+
+	private int validatePromotionStock(Product promotionProduct, int quantity) {
+		if (promotionProduct.getQuantity() < quantity) {
+			return quantity - promotionProduct.getQuantity();
+		}
+		return 0;
+	}
+
+	private void validateNormalOrderStock(Product normalProduct, int orderQuantity) {
+		if (normalProduct.getQuantity() < orderQuantity) {
+			throw new IllegalStateException(StockValidateMessage.INVALID_ORDER_OUT_OF_STOCK.getMessage());
+		}
 	}
 }
