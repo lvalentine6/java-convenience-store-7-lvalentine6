cf44710df5351c09b05b283afc0376540f053f27
lvalentine6 <ysr1227@naver.com>
2024-11-11 15:11:17 +0900

    feat: implement membership discount price feature

--- src/main/java/store/domain/membershop/Membership.java
+++ src/main/java/store/domain/membershop/Membership.java
@@ -1,4 +1,18 @@
 package store.domain.membershop;
 
 public class Membership {
+	private static final int DISCOUNT_RATE = 30;
+	private static final int MAXIMUM_DISCOUNT_AMOUNT = 8_000;
+
+	private Membership() {
+	}
+
+	public static Membership create() {
+		return new Membership();
+	}
+
+	public int calculateDiscountPrice(int price) {
+		int discountPrice = (int)(price * DISCOUNT_RATE / 100.0);
+		return Math.min(discountPrice, MAXIMUM_DISCOUNT_AMOUNT);
+	}
 }
